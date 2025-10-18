3cab782a1e827612334867054b90296e4c94571d
davidefqon <davidlarotapilco@gmail.com>
2025-10-17 19:16:43 -0500

    empezando

---
+++ config/config_inc.php
@@ -0,0 +1,12 @@
+<?php
+$g_hostname               = 'localhost';
+$g_db_type                = 'mysqli';
+$g_database_name          = 'bugtracker';
+$g_db_username            = 'root';
+$g_db_password            = '';
+
+$g_default_timezone       = 'America/Lima';
+
+$g_crypto_master_salt     = 'rgxUg4iQ0a6gGvwTy8ZTXR34v79BAnAtnov/90P6Trg=';
+
+$g_path                   = 'http://127.0.0.1/mantisbt/';
